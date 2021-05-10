6ba651afab5109a8b96321efa689f21a4848871b
qwazwsx <azimbin@gmail.com>
2021-05-09 22:52:24 -0500

    Add files via upload

---
+++ microCanvas/microCanvas.js
@@ -0,0 +1,25 @@
+// we put this on window so we can call it from the popup
+var onCanvasSave
+
+class microCanvas {
+  constructor(el, onSave) {
+    onCanvasSave = (data) => {
+      // set up previous image
+      this.img = data
+      if (onSave) onSave(data)
+    }
+
+    $(el).on('click', () => {
+      this.popup = window.open('microCanvas/popup.html', '_blank', "menubar=false, width=410, height=400")
+      // once window is done loading, set image
+      this.popup.addEventListener('load', () => {
+        this.popup.updateCanvas(this.img)
+      })
+    })
+  }
+
+  resetPreviousImage() {
+    this.img = undefined
+  }
+
+}
