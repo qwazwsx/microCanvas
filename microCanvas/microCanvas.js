4a5d5d0fc148ce85baa3930d1f067425618569a9
qwazwsx <azimbin@gmail.com>
2021-05-09 23:13:35 -0500

    update

--- microCanvas/microCanvas.js
+++ microCanvas/microCanvas.js
@@ -3,19 +3,20 @@ var onCanvasSave
 
 class microCanvas {
   constructor(el, onSave) {
+    this.el = el
     onCanvasSave = (data) => {
       // set up previous image
       this.img = data
       if (onSave) onSave(data)
     }
 
-    $(el).on('click', () => {
+    this.el.onclick = () => {
       this.popup = window.open('microCanvas/popup.html', '_blank', "menubar=false, width=410, height=400")
       // once window is done loading, set image
       this.popup.addEventListener('load', () => {
         this.popup.updateCanvas(this.img)
       })
-    })
+    }
   }
 
   resetPreviousImage() {
